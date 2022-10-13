6732c49b5e50a12108ecd9d9b6dea0577ccb08f6
iyoohana <54783797+iyoohana@users.noreply.github.com>
2022-10-13 15:49:02 +0900

    Update main.go
    edit text kor->eng

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World----! 변경"}
+		res := &response{Message: "Hello World----! TEST"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
